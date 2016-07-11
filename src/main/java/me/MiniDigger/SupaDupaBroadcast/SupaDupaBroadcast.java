b0258f3ce29b9982bf9a69d6f7c66d8539d9a79d
MiniDigger <admin@minidigger.me>
2016-07-11 21:57:07 +0200

    Add a "don't bug me " list and an option to hide console output

--- src/main/java/me/MiniDigger/SupaDupaBroadcast/SupaDupaBroadcast.java
+++ src/main/java/me/MiniDigger/SupaDupaBroadcast/SupaDupaBroadcast.java
@@ -12,6 +12,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class SupaDupaBroadcast extends JavaPlugin {
 
@@ -19,6 +21,8 @@ public class SupaDupaBroadcast extends JavaPlugin {
     private long interval;
     private BukkitRunnable task;
     private int index = 0;
+
+    private List<UUID> dontBugMe;
 
     @Override
     public void onEnable() {
@@ -32,120 +36,149 @@ public class SupaDupaBroadcast extends JavaPlugin {
     private void restartBroadcast() {
         index = 0;
 
-        if (task != null) {
+        if ( task != null ) {
             task.cancel();
         }
 
         task = new BukkitRunnable() {
             @Override
             public void run() {
-                String msg = messages.get(index);
-                if (msg.startsWith("json:")) {
-                    broadCastJson(msg);
+                String msg = messages.get( index );
+                if ( msg.startsWith( "json:" ) ) {
+                    broadCastJson( msg );
                 } else {
-                    Bukkit.broadcastMessage(parseMsg(msg));
+                    String fmsg = parseMsg( msg );
+                    Bukkit.getOnlinePlayers().stream().filter( p -> !dontBugMe.contains( p.getUniqueId() ) ).forEach( p -> p.sendMessage( fmsg ) );
+
+                    if ( getConfig().isBoolean( "console-output" ) ) {
+                        Bukkit.getConsoleSender().sendMessage( msg );
+                    }
                 }
 
                 index++;
-                if (index >= messages.size()) {
+                if ( index >= messages.size() ) {
                     index = 0;
                 }
             }
         };
-        task.runTaskTimer(this, interval, interval);
+        task.runTaskTimer( this, interval, interval );
     }
 
-    private void broadCastJson(String msg) {
-        msg = msg.replace("json:", "");
+    private void broadCastJson( String msg ) {
+        msg = msg.replace( "json:", "" );
 
 
-        BaseComponent[] bc = ComponentSerializer.parse(msg);
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            p.spigot().sendMessage(bc);
-        }
+        BaseComponent[] bc = ComponentSerializer.parse( msg );
+        Bukkit.getOnlinePlayers().stream().filter( p -> !dontBugMe.contains( p.getUniqueId() ) ).forEach( p -> p.spigot().sendMessage( bc ) );
 
         StringBuilder sb = new StringBuilder();
-        for (BaseComponent b : bc) {
-            sb.append(b.toLegacyText());
+        for ( BaseComponent b : bc ) {
+            sb.append( b.toLegacyText() );
         }
 
-        Bukkit.getConsoleSender().sendMessage(sb.toString());
+        if ( getConfig().isBoolean( "console-output" ) ) {
+            Bukkit.getConsoleSender().sendMessage( sb.toString() );
+        }
     }
 
     private void loadMessages() {
-        messages = getConfig().getStringList("messages");
-        if (messages == null || messages.size() == 0) {
-            getLogger().warning("Could not load messages from config, setting default one");
+        messages = getConfig().getStringList( "messages" );
+        if ( messages == null || messages.size() == 0 ) {
+            getLogger().warning( "Could not load messages from config, setting default one" );
             messages = new ArrayList<>();
-            messages.add("Default message: You can change this message this plugins config.yml");
-            getConfig().set("messages", messages);
+            messages.add( "Default message: You can change this message this plugins config.yml" );
+            getConfig().set( "messages", messages );
             saveConfig();
         }
 
-        interval = getConfig().getInt("interval");
-        if (interval == 0) {
-            getLogger().warning("Could not load interval from config, setting default one (" + 20 * 60 + ")");
+        interval = getConfig().getInt( "interval" );
+        if ( interval == 0 ) {
+            getLogger().warning( "Could not load interval from config, setting default one (" + 20 * 60 + ")" );
             interval = 20 * 60;
-            getConfig().set("interval", interval);
+            getConfig().set( "interval", interval );
             saveConfig();
         }
+
+        dontBugMe = new ArrayList<>();
+        dontBugMe.addAll( getConfig().getStringList( "dont-bug-me" ).stream().map( UUID::fromString ).collect( Collectors.toList() ) );
     }
 
-    private String parseMsg(String msg) {
-        msg = ChatColor.translateAlternateColorCodes('&', msg);
+    private String parseMsg( String msg ) {
+        msg = ChatColor.translateAlternateColorCodes( '&', msg );
 
         return msg;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        switch (command.getName()) {
+    public boolean onCommand( CommandSender sender, Command command, String label, String[] args ) {
+        switch ( command.getName() ) {
             case "sdbadd":
-                if (args.length == 1) {
-                    messages.add(args[0]);
-                } else if (args.length > 1) {
+                if ( args.length == 1 ) {
+                    messages.add( args[0] );
+                } else if ( args.length > 1 ) {
                     StringBuilder sb = new StringBuilder();
-                    for (String s : args) {
-                        sb.append(s);
-                        sb.append(" ");
+                    for ( String s : args ) {
+                        sb.append( s );
+                        sb.append( " " );
                     }
 
-                    messages.add(sb.toString());
+                    messages.add( sb.toString() );
                 } else {
-                    sender.sendMessage(ChatColor.RED + "You need to provide at least on argument!");
+                    sender.sendMessage( ChatColor.RED + "You need to provide at least on argument!" );
                     return true;
                 }
 
-                getConfig().set("messages", messages);
+                getConfig().set( "messages", messages );
                 saveConfig();
 
-                sender.sendMessage(ChatColor.GREEN + "Message was added and config was saved!");
+                sender.sendMessage( ChatColor.GREEN + "Message was added and config was saved!" );
                 return true;
             case "sdbrl":
                 reloadConfig();
                 loadMessages();
                 restartBroadcast();
 
-                sender.sendMessage(ChatColor.GREEN + "Reloaded!");
+                sender.sendMessage( ChatColor.GREEN + "Reloaded!" );
                 return true;
             case "sdbint":
-                if (args.length != 1) {
-                    sender.sendMessage(ChatColor.RED + "You need to specify a new interval!");
+                if ( args.length != 1 ) {
+                    sender.sendMessage( ChatColor.RED + "You need to specify a new interval!" );
                     return true;
                 }
 
                 try {
-                    interval = Integer.parseInt(args[0]);
-                } catch (NumberFormatException e) {
-                    sender.sendMessage(ChatColor.RED + "The new interval needs to be a number!");
+                    interval = Integer.parseInt( args[0] );
+                } catch ( NumberFormatException e ) {
+                    sender.sendMessage( ChatColor.RED + "The new interval needs to be a number!" );
                     return true;
                 }
 
-                getConfig().set("interval", interval);
+                getConfig().set( "interval", interval );
                 saveConfig();
                 restartBroadcast();
 
-                sender.sendMessage(ChatColor.GREEN + "Interval was set to " + interval + " and config was saved!");
+                sender.sendMessage( ChatColor.GREEN + "Interval was set to " + interval + " and config was saved!" );
+                return true;
+            case "sbignore":
+                if ( !( sender instanceof Player ) ) {
+                    sender.sendMessage( ChatColor.RED + "This command can only be executed as player!" );
+                    return true;
+                }
+
+                Player player = (Player) sender;
+
+                if ( dontBugMe.contains( player.getUniqueId() ) ) {
+                    dontBugMe.remove( player.getUniqueId() );
+                    sender.sendMessage( ChatColor.GREEN + "You will now get broadcasts again!" );
+                } else {
+                    dontBugMe.add( player.getUniqueId() );
+                    sender.sendMessage( ChatColor.GREEN + "You will not get any broadcast now!" );
+                }
+
+                List<String> l = dontBugMe.stream().map( UUID::toString ).collect( Collectors.toList() );
+                getConfig().set( "dont-bug-me", l );
+                saveConfig();
+
                 return true;
             default:
                 return false;
